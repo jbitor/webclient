d20006dd31dc54a21df9d4a74ca056ce9eb33560
Jeremy Banks <j@jeremybanks.ca>
2014-02-21 18:37:06 -0500

    Clean up use of Angular.

--- static/js/client.js
+++ static/js/client.js
@@ -4,47 +4,76 @@ angular.module('jbitor.client', [])
 
 .controller('jbitorClientController', function(
     $scope,
-    makeDHTMonitor
+    DHTModel,
+    FindPeersModel
 ) {
-    // TODO(JB): Stop passing $scope.
-    $scope.dht = makeDHTMonitor($scope);
+    $scope.dht = new DHTModel();
+    $scope.dht.startUpdating(2500, $scope);
+
+    $scope.findPeers = new FindPeersModel();
 })
 
-.factory('makeDHTMonitor', function() {
-    return function makeDHTMonitor($scope) {
-        var monitor = {
-            nodeCounts: {
-                GoodNodes: 0,
-                UnknownNodes: 0,
-                BadNodes: 0,
-            },
-            health: function() {
-                if (this.nodeCounts.GoodNodes) {
-                    return Math.min(
-                        1.0,
-                        Math.max(
-                            0.1,
-                            this.nodeCounts.GoodNodes / 32
-                        )
-                    );
-                } else if (this.nodeCounts.UnknownNodes) {
-                    return 0.05;
-                }
-            }
+.factory('FindPeersModel', function() {
+    function FindPeersModel() {
+        this.infohash = '';
+    }
+
+    FindPeersModel.prototype.onSubmit = function(event) {
+        this.infohash = '';
+        event.preventDefault();
+    }
+
+    return FindPeersModel;
+})
+
+.factory('DHTModel', function(
+    $interval
+) {
+    function DHTModel(updateInterval) {
+        this.nodeCounts = {
+            GoodNodes: 0,
+            UnknownNodes: 0,
+            BadNodes: 0,
         };
+    }
 
-        setInterval(updateNodeCounts, 1000);
-
-        function updateNodeCounts() {
-            $.getJSON('/api/nodeCounts.json').then(function(nodeCounts) {
-                $scope.$apply(function() {
-                    monitor.nodeCounts = nodeCounts;
-                });
-            }, function(err) {
-                console.error("Failed to get update node counts", err);
-            });
+    DHTModel.prototype.health = function() {
+        if (this.nodeCounts.GoodNodes) {
+            return Math.min(
+                1.0,
+                Math.max(
+                    0.1,
+                    this.nodeCounts.GoodNodes / 32
+                )
+            );
+        } else if (this.nodeCounts.UnknownNodes) {
+            return 0.05;
         }
+    }
 
-        return monitor;
-    };
+    DHTModel.prototype.startUpdating = function(interval, $scope) {
+        var that = this;
+
+        var intervalPromise = $interval(function() {
+            that.updateNodeCounts($scope);
+        }, interval);
+
+        $scope.$on('$destroy', function() {
+            $interval.cancel(intervalPromise);
+        });
+    }
+
+    DHTModel.prototype.updateNodeCounts = function($scope) {
+        var that = this;
+
+        $.getJSON('/api/nodeCounts.json').then(function(nodeCounts) {
+            $scope.$apply(function() {
+                that.nodeCounts = nodeCounts;
+            });
+        }, function(err) {
+            console.error("Failed to get update node counts", err);
+        });
+    }
+
+    return DHTModel;
 });
