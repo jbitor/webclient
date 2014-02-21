d11c77783b08878a30f3dbece0c67bbdc42ff189
Jeremy Banks <jeremy@jeremybanks.ca>
2014-02-21 02:55:03 -0500

    Improving appearance of webclient page.

--- static/js/client.js
+++ static/js/client.js
@@ -3,25 +3,48 @@
 angular.module('jbitor.client', [])
 
 .controller('jbitorClientController', function(
-    $scope
+    $scope,
+    makeDHTMonitor
 ) {
-    $scope.dht = {
-        nodeCounts: {}
-    };
+    // TODO(JB): Stop passing $scope.
+    $scope.dht = makeDHTMonitor($scope);
+})
 
-    setInterval(updateNodeCounts, 1000);
+.factory('makeDHTMonitor', function() {
+    return function makeDHTMonitor($scope) {
+        var monitor = {
+            nodeCounts: {
+                GoodNodes: 0,
+                UnknownNodes: 0,
+                BadNodes: 0,
+            },
+            health: function() {
+                if (this.nodeCounts.GoodNodes) {
+                    return Math.min(
+                        1.0,
+                        Math.max(
+                            0.1,
+                            this.nodeCounts.GoodNodes / 32
+                        )
+                    );
+                } else if (this.nodeCounts.UnknownNodes) {
+                    return 0.05;
+                }
+            }
+        };
 
-    function updateNodeCounts() {
-        console.log("Requesting updated node counts.");
+        setInterval(updateNodeCounts, 1000);
 
-        $.getJSON('/api/nodeCounts.json').then(function(nodeCounts) {
-            console.log("Got updated node counts.", nodeCounts);
-
-            $scope.$apply(function() {
-                $scope.dht.nodeCounts = nodeCounts;
+        function updateNodeCounts() {
+            $.getJSON('/api/nodeCounts.json').then(function(nodeCounts) {
+                $scope.$apply(function() {
+                    monitor.nodeCounts = nodeCounts;
+                });
+            }, function(err) {
+                console.error("Failed to get update node counts", err);
             });
-        }, function(err) {
-            console.error("Failed to get update node counts", err);
-        });
-    }
+        }
+
+        return monitor;
+    };
 });
