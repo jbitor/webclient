9ec4fc3f757afe6c169ed62fb9a1ddef71c3a152
Jeremy Banks <j@jeremybanks.ca>
2015-07-03 08:31:31 -0700

    Adding flashes of color to indicate whether a peer search request was valid or not.

--- static/scripts.js
+++ static/scripts.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   'use strict';
+
   var input = document.querySelector('input');
   input.focus();
   input.setSelectionRange(input.value.length, input.value.length);
@@ -13,14 +14,37 @@ document.addEventListener('DOMContentLoaded', function() {
     });
   }, 4000);
 
-  document.querySelector('form').addEventListener('submit', function(event) {
-    event.preventDefault();
+  function peerSearch(infohash) {
     var data = new FormData;
-    data.append('infohash', document.querySelector('input').value);
-    fetch('/api/peerSearch', {
+    data.append('infohash', infohash);
+    return fetch('/api/peerSearch', {
       method: 'POST',
       body: data
+    }).then(function(response) {
+      console.info("Started peer search.");
+    }, function(error) {
+      console.error("Unable to initiate peer search due to error:", error);
+      throw error;
     });
-    document.querySelector('input').value = '';
-  })
+  }
+
+  window.peerSearch = peerSearch;
+  console.info(
+      "Use %cpeerSearch(infohash)%c to start a search.", 'color: blue;', 'color: auto;');
+
+  document.querySelector('form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var input = document.querySelector('input');
+    input.disabled = true;
+    input.classList.remove('successful', 'error')
+    peerSearch(input.value).then(function(){
+      input.classList.add('successful');
+      input.disabled = false;
+      input.setSelectionRange(0, input.value.length);
+    }, function() {
+      input.classList.add('error');
+      input.disabled = false;
+      input.setSelectionRange(0, input.value.length);
+    });
+  });
 });
